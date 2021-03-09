9b1c7aa94805fe9b9aa58bf74af8888648f4b5b8
gperezalba <gperezalba94@gmail.com>
2021-03-09 12:52:36 +0100

    ADDED: set allowances by users array too

--- src/dex-allow.ts
+++ src/dex-allow.ts
@@ -1,20 +1,55 @@
 import { BigInt } from "@graphprotocol/graph-ts";
-import { SetAllowance } from "../generated/DEXAllow/DEXAllow";
+import { SetAllowance, SetAllowanceArray } from "../generated/DEXAllow/DEXAllow";
 import { User } from "../generated/schema";
 
 export function handleSetAllowance(event: SetAllowance): void {
-    let user = User.load(event.params.sender.toHexString());
+    let array = event.params.indexes;
+
+    if (array.includes(BigInt.fromI32(33))) {
+        updateAllowance(event.params.sender.toHexString(), event.params.allowance);
+    }
+
+    if (array.includes(BigInt.fromI32(34))) {
+        updateAllowancePackable(event.params.sender.toHexString(), event.params.allowance);
+    }
+}
+
+export function handleSetAllowanceArray(event: SetAllowanceArray): void {
+
+    let index = event.params.index;
+
+    if (index == BigInt.fromI32(33)) {
+        let array = event.params.senders;
+
+        for (let i = 0; i < array.length; i++) {
+            updateAllowance(array[i].toHexString(), event.params.allowance);
+        }
+    }
+
+    if (index == BigInt.fromI32(34)) {
+        let array = event.params.senders;
+
+        for (let i = 0; i < array.length; i++) {
+            updateAllowancePackable(array[i].toHexString(), event.params.allowance);
+        }
+    }
+}
+
+function updateAllowance(userAddr: string, allowance: boolean): void {
+    let user = User.load(userAddr);
 
     if (user != null) {
-        let array = event.params.indexes;
+        user.allowed = allowance;
 
-        if (array.includes(BigInt.fromI32(33))) {
-            user.allowed = event.params.allowance;
-        }
+        user.save();
+    }
+}
 
-        if (array.includes(BigInt.fromI32(34))) {
-            user.allowedPackable = event.params.allowance;
-        }
+function updateAllowancePackable(userAddr: string, allowance: boolean): void {
+    let user = User.load(userAddr);
+
+    if (user != null) {
+        user.allowedPackable = allowance;
 
         user.save();
     }
